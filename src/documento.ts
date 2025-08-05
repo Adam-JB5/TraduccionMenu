6c5c279cbf31568b0659626461fd7c75f61098a1
adam.janah <adam.janah@geoaitech.com>
2025-08-05 17:55:59 +0200

    Estilos incluidos, imagenes todavia no funcionales. rutas??

--- src/documento.ts
+++ src/documento.ts
@@ -4,72 +4,250 @@ import {
   Paragraph,
   TextRun,
   AlignmentType,
+  ImageRun
 } from "docx";
 import { saveAs } from "file-saver";
 
-const centered = (text: string, bold = false, size = 32) =>
+/* ********************************************** */
+
+// ESTILOS PERSONALIZADOS SEGÚN INSTRUCCIONES
+
+const imagen = (text: string) =>
   new Paragraph({
     alignment: AlignmentType.CENTER,
+    spacing: {
+      line: 360,
+      lineRule: "auto",
+    },
     children: [
       new TextRun({
         text,
-        bold,
-        size,
+        size: 14 * 2,
       }),
     ],
   });
 
-const left = (text: string, bold = false, size = 24) =>
+const menu = (text: string) =>
   new Paragraph({
-    alignment: AlignmentType.LEFT,
+    alignment: AlignmentType.CENTER,
+    spacing: {
+      line: 360,
+      lineRule: "auto",
+    },
     children: [
       new TextRun({
         text,
-        bold,
-        size,
+        bold: true,
+        underline: {},
+        size: 20 * 2,
+        font: "Century Gothic",
       }),
     ],
   });
+
+const aperitivo = (text: string) =>
+  new Paragraph({
+    alignment: AlignmentType.CENTER,
+    spacing: {
+      line: 360,
+      lineRule: "auto",
+    },
+    children: [
+      new TextRun({
+        text,
+        size: 12 * 2,
+        font: "Century Gothic",
+      }),
+    ],
+  });
+
+const simbolo = (text: string) =>
+  new Paragraph({
+    alignment: AlignmentType.CENTER,
+    spacing: {
+      line: 360,
+      lineRule: "auto",
+    },
+    children: [
+      new TextRun({
+        text,
+        size: 15 * 2,
+        font: "Century Gothic",
+      }),
+    ],
+  });
+
+const normal = (text: string) =>
+  new Paragraph({
+    alignment: AlignmentType.CENTER,
+    spacing: {
+      line: 360,
+      lineRule: "auto",
+    },
+    children: [
+      new TextRun({
+        text,
+        size: 12 * 2,
+        font: "Century Gothic",
+      }),
+    ],
+  });
+
+const bodega = (text: string) =>
+  new Paragraph({
+    alignment: AlignmentType.CENTER,
+    spacing: {
+      line: 360,
+      lineRule: "auto",
+    },
+    children: [
+      new TextRun({
+        text,
+        bold: true,
+        italics: true,
+        size: 11 * 2,
+        font: "Century Gothic",
+      }),
+    ],
+  });
+
+const precio = (text: string) =>
+  new Paragraph({
+    alignment: AlignmentType.CENTER,
+    spacing: {
+      line: 360,
+      lineRule: "auto",
+    },
+    children: [
+      new TextRun({
+        text,
+        bold: true,
+        size: 11 * 2,
+        font: "Century Gothic",
+      }),
+    ],
+  });
+
+const horario = (text: string) =>
+  new Paragraph({
+    alignment: AlignmentType.CENTER,
+    spacing: {
+      line: 360,
+      lineRule: "auto",
+    },
+    children: [
+      new TextRun({
+        text,
+        bold: true,
+        underline: {},
+        size: 11 * 2,
+        font: "Century Gothic",
+      }),
+    ],
+  });
+
+const final = (text: string) =>
+  new Paragraph({
+    alignment: AlignmentType.CENTER,
+    spacing: {
+      line: 360,
+      lineRule: "auto",
+    },
+    children: [
+      new TextRun({
+        text,
+        bold: true,
+        italics: true,
+        size: 10 * 2,
+        font: "Century Gothic",
+        color: "0000ff",
+      }),
+    ],
+  });
+
+const espacios = (text: string) =>
+  new Paragraph({
+    alignment: AlignmentType.CENTER,
+    spacing: {
+      line: 360,
+      lineRule: "auto",
+    },
+    children: [
+      new TextRun({
+        text,
+        size: 10 * 2,
+        font: "Century Gothic",
+      }),
+    ],
+  });
+
+/* ********************************************** */
+
+/* const logo = await fetch("logo.jpg").then(res => res.arrayBuffer());
+const qr = await fetch("/src/qr.png").then(res => res.arrayBuffer()); */
+
 
 const generateDoc = async () => {
   const doc = new Document({
     sections: [
       {
         children: [
-          centered("MENÚ ESPECIAL", true, 40),
-          new Paragraph(""),
-          centered("APERITIVO DE BIENVENIDA", true),
-          centered("****"),
-          centered("*ENSALADA DE LANGOSTINOS Y SALMÓN CON ALIÑO DE MIEL Y MOSTAZA"),
-          centered("SOBRE TULIPA DE PASTA FILO"),
-          centered("*PUERROS CONFITADOS CON ESPUMA DE AJOBLANCO"),
-          centered("*ARROZ NEGRE CON JIBION EN SU TINTA"),
-          centered("*VOLOVAN RELLENO DE PUERROS Y GAMBAS"),
-          centered("*CAZUELITA DE ALMEJAS Y MEJILLONES EN SALSA MARINERA"),
-          centered("*MARMITAKO DE BONITO"),
-          centered("............................."),
-          centered("*BACALAO A LA VIZCAÍNA"),
-          centered("*MERLUZA BRASEADA CON SU REFRITO Y PATATA PANADERA"),
-          centered("*BONITO DE TEMPORADA ENCEBOLLADO"),
-          centered("*ENTRECOT GRILLÉ CON PATATAS FRITAS Y PIMIENTOS DEL PAÍS"),
-          centered("*RABO ESTOFADO EN SU JUGO CON PARMENTIER"),
-          centered("*CONFIT DE PATO CON COMPOTA DE MANZANA A LA REDUCCIÓN DE PEDRO XIMÉNEZ"),
-          centered("..................................."),
-          centered("*SURTIDO DE POSTRES CASEROS"),
-          new Paragraph(""),
-          centered("BODEGA NO INCLUIDA"),
-          new Paragraph(""),
-          centered("PRECIO: 34,00 € IVA INCLUIDO", true),
-          centered("1 SOLO PLATO Y POSTRE...........24,00 € IVA INCLUIDO", true),
-          new Paragraph(""),
-          centered("HORARIO RESTAURANTE:", true),
-          centered("ALMUERZO DE 13:15 H. A 15:30 H. // CENA DE 20:30 H. A 22:30 H."),
-          new Paragraph(""),
-          centered(
-            "ESTE ESTABLECIMIENTO NO PUEDE GARANTIZAR LA CONTAMINACIÓN CRUZADA DE NINGÚN ALÉRGENO POR TRAZABILIDAD EN LA ELABORACIÓN DE LOS PRODUCTOS DE NUESTRA CARTA Y MENÚS.",
-            false,
-            20
-          ),
+          /* new Paragraph({
+            alignment: AlignmentType.CENTER,
+            children: [
+              new ImageRun({
+                type: "jpg",
+                data: logo,
+                transformation: {
+                  width: 300,
+                  height: 150,
+                },
+              }),
+            ],
+          }),
+          new Paragraph({
+            alignment: AlignmentType.CENTER,
+            children: [
+              new ImageRun({
+                type: "png",
+                data: qr,
+                transformation: {
+                  width: 300,
+                  height: 150,
+                },
+              }),
+            ],
+          }), */
+          menu("MENÚ ESPECIAL"),
+          espacios(""),
+          aperitivo("APERITIVO DE BIENVENIDA"),
+          simbolo("****"),
+          normal("*ENSALADA DE LANGOSTINOS Y SALMÓN CON ALIÑO DE MIEL Y MOSTAZA"),
+          normal("SOBRE TULIPA DE PASTA FILO"),
+          normal("*PUERROS CONFITADOS CON ESPUMA DE AJOBLANCO"),
+          normal("*ARROZ NEGRE CON JIBION EN SU TINTA"),
+          normal("*VOLOVAN RELLENO DE PUERROS Y GAMBAS"),
+          normal("*CAZUELITA DE ALMEJAS Y MEJILLONES EN SALSA MARINERA"),
+          normal("*MARMITAKO DE BONITO"),
+          simbolo("............................."),
+          normal("*BACALAO A LA VIZCAÍNA"),
+          normal("*MERLUZA BRASEADA CON SU REFRITO Y PATATA PANADERA"),
+          normal("*BONITO DE TEMPORADA ENCEBOLLADO"),
+          normal("*ENTRECOT GRILLÉ CON PATATAS FRITAS Y PIMIENTOS DEL PAÍS"),
+          normal("*RABO ESTOFADO EN SU JUGO CON PARMENTIER"),
+          normal("*CONFIT DE PATO CON COMPOTA DE MANZANA A LA REDUCCIÓN DE PEDRO XIMÉNEZ"),
+          simbolo("..................................."),
+          normal("*SURTIDO DE POSTRES CASEROS"),
+          espacios(""),
+          bodega("BODEGA NO INCLUIDA"),
+          espacios(""),
+          precio("PRECIO: 34,00 € IVA INCLUIDO"),
+          precio("1 SOLO PLATO Y POSTRE...........24,00 € IVA INCLUIDO"),
+          espacios(""),
+          horario("HORARIO RESTAURANTE:"),
+          horario("ALMUERZO DE 13:15 H. A 15:30 H. // CENA DE 20:30 H. A 22:30 H."),
+          espacios(""),
+          final("ESTE ESTABLECIMIENTO NO PUEDE GARANTIZAR LA CONTAMINACIÓN CRUZADA DE NINGÚN ALÉRGENO POR TRAZABILIDAD EN LA ELABORACIÓN DE LOS PRODUCTOS DE NUESTRA CARTA Y MENÚS."),
         ],
       },
     ],
